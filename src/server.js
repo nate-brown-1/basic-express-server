5d9fbd5485b55bed54d4d44d0cf1613a2cbe9d99
Nate Brown <nathan.d.brown@gmail.com>
2023-04-25 23:39:04 -0700

    builds now, test message

--- src/server.js
+++ src/server.js
@@ -8,53 +8,29 @@ const app = express();
 const cors = require('cors');
 app.use(cors());
 
-// import function from lab 01
-// this is only for sample purposes
-const uncapitalizeMessage = require('./uncapitalize/uncapitalizeMessage');
+// // import logger module
+// const logger = require('./middleware/logger');
+// app.use(logger);
 
-// import logger module
-const logger = require('./middleware/logger');
-app.use(logger);
+// // import validator module
+// const validator = require('./middleware/validator');
+// app.use(validator);
 
-// import validator module
-const validator = require('./middleware/validator');
-app.use(validator);
+// // error handler modules
+// const notFoundErrorHandler = require('./error-handlers/404');
+// const serverErrorHandler = require('./error-handlers/500');
 
-// error handler modules
-const notFoundErrorHandler = require('./error-handlers/404');
-const serverErrorHandler = require('./error-handlers/500');
-
-// empty data array for later
-const data = [];
-
-
-app.get('/status', (request, response, next) => {
-  response.send(200, 'Great Job!');
+app.get('/test', (request, response, next) => {
+  response.status(200).send('Test Passed! Great Job!');
 });
 
-// person route
-app.get('/person', (request, response, next) => {
-  if ( validator(request.query.name) ) {
-    response.send(200, {'name': `${request.query.name}`});
-  }
-});
-
-// app.get('/message', (request, response, next) => {
-//   response.send(data);
+// // error 404 for bad routes
+// app.get('*', (request, response, next) => {
+//   notFoundErrorHandler();
 // });
 
-// app.post('/message', uncapitalizeMessage, (request, response, next) => {
-//   data.push(request.query.message);
-//   response.json(data);
-// });
-
-// error 404 for bad routes
-app.get('*', (request, response, next) => {
-  notFoundErrorHandler();
-});
-
-// last chance, error 500
-app.use(serverErrorHandler);
+// // last chance, error 500
+// app.use(serverErrorHandler);
 
 // export server object with app and start
 module.exports = {
